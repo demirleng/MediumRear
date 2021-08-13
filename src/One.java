f6615cf6f3d577d44d680420f8e09f4683c1f2bb
demirleng <88108454+demirleng@users.noreply.github.com>
2021-08-13 00:31:38 -0400

    Update One.java

--- src/One.java
+++ src/One.java
@@ -3,6 +3,9 @@ public class One {
 
     public static void main(String[] args) {
         System.out.println("I like to eat meat");
+        System.out.println("I like to eat meat");
+        System.out.println("I like to eat meat");
+        System.out.println("I like to eat meat");
     }
 
 }
